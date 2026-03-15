31d6c7b0c863961124d9ad38b2ec8d2f7d2f5a37
bapaipaul999 <rdbapaipaul@gmail.com>
2026-03-15 21:22:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (21.10%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int y = n - 999;
+        if (n<1000){
+            
+            return 0;
+        }
+        return y;
+        
+    }
+};
