434094341ceb52646e79acccd7c51e97dcbf4880
bapaipaul999 <rdbapaipaul@gmail.com>
2026-03-15 21:48:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (80.83%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans = "";
+        int level = 0;
+        for(auto ch : s){
+            if(ch == '('){
+                if(level > 0){
+                    ans += ch;
+                }
+                level ++;
+
+            }
+            else if(ch == ')'){
+                level--;
+                if(level > 0){
+                    ans += ch;
+                } 
+            }
+        }
+        return ans;
+    }
+};
