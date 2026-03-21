6670d16ee48c4e74a5d0f39f7469583affd40319
bapaipaul999 <rdbapaipaul@gmail.com>
2026-03-21 23:46:06 +0530

    Time: 0 ms (100.00%) | Memory: 33.1 MB (55.13%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -3,11 +3,8 @@ public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int>ans;
         for(int i = 0 ; i< words.size() ; i++){
-            for(auto ch : words[i]){
-                if(ch == x){
-                    ans.push_back(i);
-                    break;
-                }
+            if (words[i].find(x) != string::npos) {
+                ans.push_back(i);
             }
         }
         return ans;
