382ff6987f9c0506fb8fcad7985ff4cb927094c0
bapaipaul999 <rdbapaipaul@gmail.com>
2026-06-06 01:04:56 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (19.55%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<char>> ref(m , vector<char>(n,'X'));
+        vector<vector<int>> visited(m , vector<int>(n,0));
+
+        queue<pair<int,int>>q1;
+        for(int i = 0 ; i< m ; i++){
+            for(int j = 0 ; j<n ; j++){
+                if((i == 0 || i == m-1 || j == 0 || j == n-1)&&board[i][j] == 'O'){
+                    q1.push({i,j});
+                    visited[i][j] = 1;
+                }
+            }
+
+        }
+        vector<int> sr = {-1,0,1,0};
+        vector<int> sc = {0,1,0,-1};
+        while(!q1.empty()){
+            int row = q1.front().first;
+            int col = q1.front().second;
+            q1.pop();
+            ref[row][col] = 'O';
+            for(int i = 0; i<4 ; i++){
+                int newrow = row + sr[i];
+                int newcol = col +sc[i];
+                if(newrow >= 0 && newrow < m &&
+                   newcol >= 0 && newcol < n&&
+                   visited[newrow][newcol] == 0&&board[newrow][newcol] == 'O' ){
+                        q1.push({newrow , newcol});
+                        visited[newrow][newcol] = 1;
+                   }
+            }
+        }
+        board = ref;
+    }
+};
