5690f434afb7753e1fbaa353364bf949acda28a8
bapaipaul999 <rdbapaipaul@gmail.com>
2026-06-16 18:38:19 +0530

    Time: 3 ms (81.30%) | Memory: 19 MB (80.53%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int count = 0;
+        for(auto it : s){
+            if(it == 'a' || it == 'e' || it == 'i' || it == 'o' ||it == 'u'){
+                count ++;
+            }
+        }
+        if(count%2 == 1){
+            return true;
+        }
+        if(count==0){
+            return false;
+        }
+        return true;
+    }
+};
