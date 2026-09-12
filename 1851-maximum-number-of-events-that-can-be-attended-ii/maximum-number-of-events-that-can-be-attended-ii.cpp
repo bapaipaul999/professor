542d8d7510aa80de756f495dda778d05ac7d4bb9
bapaipaul999 <rdbapaipaul@gmail.com>
2026-09-12 16:57:19 +0530

    Time: 171 ms (81.47%) | Memory: 190.9 MB (9.45%) - LeetSync

---
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    long long f(int i, int k, vector<vector<long long>>& dp,
+                vector<vector<int>>& events, vector<int>& next) {
+
+        if (i == events.size() || k == 0)
+            return 0;
+
+        if (dp[i][k] != -1)
+            return dp[i][k];
+
+        long long notTake = f(i + 1, k, dp, events, next);
+
+        long long take = events[i][2] + f(next[i], k - 1, dp, events, next);
+
+        return dp[i][k] = max(take, notTake);
+    }
+
+    int maxValue(vector<vector<int>>& events, int k) {
+        int n = events.size();
+
+        sort(events.begin(), events.end());
+
+        vector<int> start(n);
+
+        for (int i = 0; i < n; i++)
+            start[i] = events[i][0];
+
+        vector<int> next(n);
+
+        for (int i = 0; i < n; i++) {
+
+            int l = i + 1;
+            int r = n;
+
+            while (l < r) {
+
+                int mid = l + (r - l) / 2;
+
+                if (start[mid] > events[i][1])
+                    r = mid;
+                else
+                    l = mid + 1;
+            }
+
+            next[i] = l;
+        }
+
+        vector<vector<long long>> dp(n + 1, vector<long long>(k + 1, -1));
+
+        return f(0, k, dp, events, next);
+    }
+};
