94e4f8c160bfdbc979c177debca34e7d15300c0f
BAPAI PAUL <rdbapaipaul@gmail.com>
2026-09-22 19:25:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (79.46%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    int partition(vector<int>& nums , int low , int high){
-        int pivot = nums[low];
-        int i = low;
-        int j = high;
-        while(i<j){
-            while(nums[i] <= pivot && i <= high-1){
-                i++;
-            }
-            while(nums[j] > pivot && j >= low+1){
-                j--;
-            }
-            if(i<j){
-                swap(nums[i] , nums[j]);
-            }
-
-        }
-        swap(nums[j],nums[low]);
-        return j;
-    }
-    void quicksort(vector<int>& nums , int low , int high){
-        if(low<high){
-            int pIndex = partition(nums,low,high);
-            quicksort(nums,low,pIndex-1);
-            quicksort(nums,pIndex+1,high);
-        }
-    }
     void sortColors(vector<int>& nums) {
-        int n = nums.size() - 1;
-        quicksort(nums,0,n);
+        int l = 0;
+        int mid = 0;
+        int r = nums.size() - 1;
+
+        while (mid <= r) {
+            if (nums[mid] == 0) {
+                swap(nums[l], nums[mid]);
+                l++;
+                mid++;
+            }
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+            else {
+                swap(nums[mid], nums[r]);
+                r--;
+            }
+        }
     }
 };
